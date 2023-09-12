1d031816bee3227645f7efee82094521d8e563fc
apha-pesso <mutiuayo99@gmail.com>
2023-09-12 21:39:56 +0100

    Simple User CRUD

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/test";
-const uri =
-  "mongodb+srv://pesso:hngXnation@hngxcluster.g0hliwi.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/test";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
